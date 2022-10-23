689245f6c7e35affc019605c902e876beea508fa
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2022-10-23 21:25:04 +0900

    [modify]column

--- R/location.R
+++ R/location.R
@@ -4,17 +4,20 @@
 jartic_type_b_loc_tiny <- function(data) {
   location_name <- NULL
   d <-
-    unique(data[, c("source_code",
-                    "location_no",
-                    "location_name",
-                    "meshcode10km"),
+    unique(data[, jartic_vars$type_B$loc,
                 with = FALSE],
-           by = c("source_code",
-                  "location_no",
-                  "location_name",
-                  "meshcode10km"))
+           by = jartic_vars$type_B$loc)
   tidyr::separate(d,
                   location_name,
                   into = c("location_from", "location_to"),
                   sep = "\u2192")
 }
+
+jartic_vars <-
+  list(
+    type_B = list(core = c("datetime", "location_no", "traffic"),
+                  loc = c("source_code",
+                          "location_no",
+                          "location_name",
+                          "meshcode10km"))
+  )
