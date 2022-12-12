07bec3188889f7ca585d6b6e5f1981967588f7a7
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2022-12-12 22:47:45 +0900

    [add]jartic provider
    データ提供元の県警の一覧

---
+++ R/data-jartic_provider.R
@@ -0,0 +1,10 @@
+#' Jartic data provider
+#' @description data provider list
+#' @format A tibble with 51 rows 2 variables:
+#' \itemize{
+#'   \item{name: prefecture}
+#'   \item{name_en: roman}
+#' }
+#' @examples
+#' jartic_provider
+"jartic_provider"
